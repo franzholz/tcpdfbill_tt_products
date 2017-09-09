0c4bf9dbe338abea5b0cb5ff0e435741aea5de20
Franz Holzinger <holzinger@ttproducts.de>
2017-09-09 20:26:17 +0200

    remove ext_autoload.php because there is a composer.json. remove the autoload part from ext_emconf.php for the same reason

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.3',
+	'version' => '0.0.4',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-7.99.99',
@@ -39,11 +39,5 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 		'suggests' => array(
 		),
-	),
-	'suggests' => array(
-	),
-	'autoload' =>
-	array(
-		'psr-4' => array('JambageCom\\TcpdfbillTtProducts\\' => 'Classes')
 	),
 );
