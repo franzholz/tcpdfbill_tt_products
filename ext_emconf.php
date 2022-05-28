ad2c1ddb9d19a8044830e965c8549024be1c0dfb
Franz Holzinger <franz.holzinger@festwein.de>
2022-05-28 19:35:48 +0200

    * support for TYPO3 11.5 next version 0.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TCPDF bill for tt_products',
-    'description' => 'tt_products Extension with automatic PDF bill generation using the TCPDF library. Works with tt_products 2.8.1-3.2.99',
+    'description' => 'tt_products Extension with automatic PDF bill generation using the TCPDF library. Works with tt_products 2.9.20-3.2.99',
     'category' => 'fe',
     'author' => 'Franz Holzinger',
     'author_email' => 'franz@ttproducts.de',
@@ -15,17 +15,18 @@ $EM_CONF[$_EXTKEY] = [
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
     'author_company' => '',
-    'version' => '0.2.0',
+    'version' => '0.3.0',
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-7.99.99',
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.0-11.5.99',
 			'div2007' => '1.10.27-0.0.0',
         ],
         'conflicts' => [
         ],
         'suggests' => [
             't3_tcpdf' => '5.1.1-0.0.0',
+            'tcpdf' => '3.0.0-0.0.0',
         ],
     ],
 ];
